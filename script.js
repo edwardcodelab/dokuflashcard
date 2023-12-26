6c2c4df13a127186f2403883ade4afa521b4c1fa
edwardcodelab <56959323+edwardcodelab@users.noreply.github.com>
2023-12-26 17:48:43 +0800

    v1.0

---
+++ script.js
@@ -0,0 +1,58 @@
+
+    let cardcount = 1;
+	let frontback = 1;
+    let cardarray = new Array();
+	let cardarraybr = new Array();
+	
+
+    function flipcard(){
+	
+
+    if(frontback === 0){
+
+	   jQuery("#cardpanel").addClass('unflipped').removeClass('flipped');
+	   jQuery("#cardpanel").text(cardarraybr[cardcount][0]); 
+	   
+	   frontback = 1;
+	   console.log(cardcount);
+	   console.log(cardarraybr[cardcount][0]);
+	  
+    }
+    else
+    {
+
+	   jQuery("#cardpanel").text(cardarraybr[cardcount][1]); 
+	   jQuery("#cardpanel").addClass('flipped').removeClass('unflipped');
+	   frontback = 0;
+	   console.log(cardcount);
+	   console.log(cardarraybr[cardcount][1]);
+	   
+    }
+	
+}
+
+
+function newcard(){
+  cardcount = cardcount +1;
+  jQuery("#cardpanel").text(cardarraybr[cardcount][0]); 
+  jQuery("#cardpanel").addClass('unflipped').removeClass('flipped');
+  frontback = 1
+}
+
+function oldcard(){
+  cardcount = cardcount -1;
+  jQuery("#cardpanel").text(cardarraybr[cardcount][0]); 
+  jQuery("#cardpanel").addClass('unflipped').removeClass('flipped');
+  frontback = 1
+}
+
+function startcard(){
+	
+		console.log(jQuery('#carddata').text());
+	cardarray = jQuery('#carddata').text().split("^");
+	console.log(cardarray);
+	cardarraybr = cardarray.map((value) => value.split('|'));
+	console.log(cardarraybr);
+	jQuery("#cardpanel").text(cardarraybr[cardcount][0]); 
+	
+}
---
+++ syntax.php
@@ -0,0 +1,115 @@
+<?php
+
+use dokuwiki\Extension\SyntaxPlugin;
+
+/**
+ * DokuWiki Plugin dokuflashcard (Syntax Component)
+ *
+ * @license GPL 2 http://www.gnu.org/licenses/gpl-2.0.html
+ * @author dodotori@dokuwikiforum <dodotori@dokuwikiforum>
+ */
+class syntax_plugin_dokuflashcard extends SyntaxPlugin
+{
+	
+	
+
+	
+	
+	
+	
+	
+	
+    /** @inheritDoc */
+    public function getType()
+    {
+        return 'protected';
+    }
+
+    /** @inheritDoc */
+    public function getPType()
+    {
+        return 'block';
+    }
+
+    /** @inheritDoc */
+    public function getSort()
+    {
+        return 32;
+    }
+
+    /** @inheritDoc */
+    public function connectTo($mode)
+    {
+       // $this->Lexer->addSpecialPattern('<FIXME>', $mode, 'plugin_dokuflashcard');
+        $this->Lexer->addEntryPattern('<flashcard>', $mode, 'plugin_dokuflashcard');
+    }
+
+//    /** @inheritDoc */
+    public function postConnect()
+    {
+        $this->Lexer->addExitPattern('</flashcard>', 'plugin_dokuflashcard');
+    }
+
+    /** @inheritDoc */
+    public function handle($match, $state, $pos, Doku_Handler $handler)
+    {
+        
+	
+		switch ($state) {
+          case DOKU_LEXER_ENTER :
+                return array($state, '');
+ 
+          case DOKU_LEXER_UNMATCHED :  
+				return array($state, $match);
+				
+				
+          case DOKU_LEXER_EXIT :       
+				return array($state, '');
+        }
+
+
+	
+        return array();
+		
+		
+		
+		
+    }
+
+    /** @inheritDoc */
+    public function render($mode, Doku_Renderer $renderer, $data)
+    {
+        if ($mode !== 'xhtml') {
+            return false;
+        }
+
+		
+		list($state,$match) = $data;
+            switch ($state) {
+                case DOKU_LEXER_ENTER :      
+                    $renderer->doc .= '<div id="cardpanel" class="unflipped"  onclick="flipcard()">Start!</div>';
+					$renderer->doc .= '
+					<div class="fbuttoncontainer">
+					<button class="flashcardbutton" onclick="oldcard()">back</button>
+					<button class="flashcardbutton" onclick="startcard()">start</button>
+					<button class="flashcardbutton" onclick="newcard()">next</button>
+					</div>';
+					$renderer->doc .= '<div id = "carddata">'; 
+                    break;
+ 
+                case DOKU_LEXER_UNMATCHED :  
+					
+                    $renderer->doc .= $renderer->_xmlEntities($match); 
+					
+                    break;
+                case DOKU_LEXER_EXIT :       
+                    $renderer->doc .= '</div>'; 
+                    break;
+		
+		
+		
+		
+        return true;
+    }
+	}
+}
